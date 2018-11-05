ab1801d7b3159d38604f7003e498934869a8f70f
root <slingmint@gmail.com>
2018-11-05 17:05:18 -0600

    added new shit

---
+++ app/main.js
@@ -0,0 +1,14 @@
+module.exports = (args, chalk) => {
+	console.log(chalk.red('________________________________________________________________'))
+	console.log(chalk.red('|                                                              |'))
+	console.log(chalk.red('|                                                              |'))
+	console.log(chalk.red('|                                                              |'))
+	console.log(chalk.red('|                                                              |'))
+	console.log(chalk.red('|                                                              |'))
+	console.log(chalk.red('|                                                              |'))
+	console.log(chalk.red('|                                                              |'))
+	console.log(chalk.red('|______________________________________________________________|'))
+
+
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk')
+const minimist = require('minimist')
 
 module.exports = () => {
+	const args = minimist(process.argv.slice(2))
 	console.log(chalk.green("Test Program"))
+	console.log(args)
+
+	if (args.v || args.version) {
+		console.log(chalk.red('version'))
+		console.log(chalk.red(args.version))
+	}
+
+	require('./app/main.js')(args, chalk)
 }
 
