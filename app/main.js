41b6abda8103644e40fb4f65ebdeb756cfec9149
root <slingmint@gmail.com>
2018-11-06 15:05:12 -0600

    added new files

--- app/main.js
+++ app/main.js
@@ -1,14 +1,4 @@
+
 module.exports = (args, chalk) => {
-	console.log(chalk.red('________________________________________________________________'))
-	console.log(chalk.red('|                                                              |'))
-	console.log(chalk.red('|                                                              |'))
-	console.log(chalk.red('|                                                              |'))
-	console.log(chalk.red('|                                                              |'))
-	console.log(chalk.red('|                                                              |'))
-	console.log(chalk.red('|                                                              |'))
-	console.log(chalk.red('|                                                              |'))
-	console.log(chalk.red('|______________________________________________________________|'))
-
-
-
+	
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const minimist = require('minimist')
+const boxen = require('boxen')
 
 module.exports = () => {
 	const args = minimist(process.argv.slice(2))
@@ -12,5 +13,44 @@ module.exports = () => {
 	}
 
 	require('./app/main.js')(args, chalk)
+
+	var c = ''	
+	getData().forEach(function(item) {
+		c += getBoxen(item)
+	})
+
+	console.log(boxen(c, {padding: 5, align: 'left'}))
 }
 
+
+function getBoxen(itemObject) {
+	var formatObject = {padding: 1, borderStyle: 'round', borderColor: 'black', backgroundColor: 'red'}
+	return boxen(itemObject.start + ' - ' + itemObject.end + ' : ' + itemObject.subject, formatObject)	
+
+}
+
+function getData() {
+	return [
+		{
+			'start':'08:00',
+		 	'end': '09:00',
+		 	'subject' : 'This is an appointment at the beginning'
+		},
+		{
+			'start':'09:00',
+			'end':'11:00',
+			'subject': 'Second Item To View'
+		},
+		{
+			'start': '11:00',
+			'end' : '11:30',
+			'subject' : 'Test Item Number 3 To Look Into'
+		},
+		{
+			'start' : '11:30',
+			'end': '13:30',
+			'subject' : 'Final item in Day'
+		}
+	]
+
+}
