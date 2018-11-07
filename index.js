62c94b299408809e19068f72d4b0ac6298e7ab5d
root <slingmint@gmail.com>
2018-11-07 17:17:58 -0600

    new files

--- index.js
+++ index.js
@@ -1,27 +1,103 @@
+const DATAFILE = '/root/.cl-data'
 const chalk = require('chalk')
-const minimist = require('minimist')
 const boxen = require('boxen')
+const vorpal = require('vorpal')()
+const fs = require('fs')
+var data = []  //= initializeData()
 
 module.exports = () => {
-	const args = minimist(process.argv.slice(2))
-	console.log(chalk.green("Test Program"))
-	console.log(args)
 
-	if (args.v || args.version) {
-		console.log(chalk.red('version'))
-		console.log(chalk.red(args.version))
-	}
+//	require('./app/main.js')(chalk)
 
-	require('./app/main.js')(args, chalk)
+	vorpal
+		.command('add <begin> <end> <subject>', 'Adds A New Entry')
+		.option('-b, --begin <begin>', 'Begin Time')
+		.option('-e, --end <end>', 'End Time')
+		.option('-s, --subject <subject>', 'Subject')
+		.action(function(args, callback) {
+//			if ((args.options.begin) && (args.options.end) && (args.options.subject)) {
+				addEntry(args)
+				refreshView()
+//			} else {
+//				refreshView()
+//				console.log ("Invalid options.  See help.")
+//			}
+			callback()
+		})
 
-	var c = ''	
-	getData().forEach(function(item) {
-		c += getBoxen(item)
-	})
 
-	console.log(boxen(c, {padding: 5, align: 'left'}))
+	vorpal
+		.command('refresh', 'Refreshes the screen')
+		.alias('r')
+		.action(function(args, callback) {
+			refreshView()
+			callback()
+		})
+
+
+
+	vorpal
+		.command('load', 'Loads last save file')
+		.action(function(args, callback) {
+			data = fs.readFileSync(DATAFILE, 'utf-8')
+			data =  JSON.parse(data)
+			refreshView()
+			callback()
+		})
+
+	vorpal
+		.command('save', 'Saves to file')
+		.action(function(args, callback) {
+			var jsondata = JSON.stringify(data)
+
+			fs.writeFile(DATAFILE, jsondata, 'utf-8', function(err) {
+				if (err) {
+					console.log("Error saving file.")
+					return console.log(err)
+				}
+			})	
+			console.log(chalk.red('Datafile saved.'))
+			callback()		
+		})
+
+
+
+//	refreshView()
+
+
+	vorpal
+		.delimiter('CL-CAL#')
+		.show()
 }
 
+
+function refreshView() {
+ 	console.log('\033c')	
+	var c = getHeader() 
+	getData().forEach(function(item, index) {
+		c += '\n' + getBoxen(item)
+	})
+
+	console.log(boxen(c, {padding: 0, align: 'left'}))
+}
+
+//function UseLaterpromptItem(v) {
+//	self = v
+//	self.prompt( {
+//		type: 'input',
+//		name: 'start',
+//		message: 'Start Date: '
+//	},
+//	function (result) {
+//		console.log('did something')
+//	})
+//}
+function getHeader() {
+	var formatObject = {align: 'left', float: 'left', padding: 3, borderStyle: 'round', borderColor: 'black', backgroundColor: 'blue'}
+
+	return boxen("-----------------------------------------------------" + '\n' + 'asdf' + '\n' +
+                     "-----------------------------------------------------", formatObject)
+}
 
 function getBoxen(itemObject) {
 	var formatObject = {padding: 1, borderStyle: 'round', borderColor: 'black', backgroundColor: 'red'}
@@ -29,7 +105,32 @@ function getBoxen(itemObject) {
 
 }
 
+function addEntry(options) {
+	var a = options.begin.toString().replace(/\b(\d{1,2})(\d{2})/g, '$1:$2') 
+	var b = options.end.toString().replace(/\b(\d{1,2})(\d{2})/g, '$1:$2') 
+
+	var entry = {
+		start: a,
+		end: b,
+		subject: options.subject
+	} 
+console.log(entry)
+	data.push(entry)
+}
+
+
 function getData() {
+
+	return data.sort(function(a,b) {
+		var da = new Date('1970-01-01T' + a.start + 'Z')
+		var db = new Date('1970-01-01T' + b.start + 'Z')
+
+		return (da.getTime() > db.getTime()) ? 1 : ((db.getTime() > da.getTime()) ? -1 : 0)
+	})
+}
+
+
+function initializeData() {
 	return [
 		{
 			'start':'08:00',
@@ -50,6 +151,26 @@ function getData() {
 			'start' : '11:30',
 			'end': '13:30',
 			'subject' : 'Final item in Day'
+		},
+		{
+			'start' : '13:30',
+			'end': '13:45',
+			'subject' : 'Test Another Line'
+		},
+		{
+			'start' : '13:45',
+			'end' : '14:00',
+			'subject' : 'Another Test Line To see length limitations'
+		},
+		{
+			'start' : '14:00',
+			'end' : '14:15',
+			'subject': 'Hello world again'
+		},
+		{
+			'start' : '14:14',
+			'end' : '14:30',
+			'subject' : 'Hwllo world towards the end'
 		}
 	]
 
